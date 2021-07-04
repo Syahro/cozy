eb46c36092f8e6a8d993cd7cb960aa209d97d038
Arief Syahroni <riefsyah31@gmail.com>
2021-07-04 21:59:31 +0700

    done modified splash page

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Color purpleColor = Color(0XFF5843BE);
 Color orangeColor = Color(0XFFFF9376);
 Color blackColor = Color(0XFF000000);
 Color whiteColor = Color(0XFFFFFFFF);
-Color darkGreyColor = Color(0XFFFF9376);
+Color darkGreyColor = Color(0XFF82868E);
 Color lightGreyColor = Color(0XFFF6F7F8);
+Color greyColor = Color(0XFF989BA1);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: blackColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: whiteColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w300,
+  color: darkGreyColor,
+);
+
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: purpleColor,
+);

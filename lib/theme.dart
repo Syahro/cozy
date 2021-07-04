1cfbaf3c6ce4e68e7076f83895ac22a49f1d3f97
Arief Syahroni <riefsyah31@gmail.com>
2021-07-04 21:26:58 +0700

    Add new Theme, some assets, and regist library

---
+++ lib/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color purpleColor = Color(0XFF5843BE);
+Color orangeColor = Color(0XFFFF9376);
+Color blackColor = Color(0XFF000000);
+Color whiteColor = Color(0XFFFFFFFF);
+Color darkGreyColor = Color(0XFFFF9376);
+Color lightGreyColor = Color(0XFFF6F7F8);

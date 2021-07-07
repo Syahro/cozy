fc5fa7accd3589a7a3a6dc7ed4790869d3cdcb66
Arief Syahroni <riefsyah31@gmail.com>
2021-07-07 14:13:28 +0700

    Modified Splash and Home to add background, then Modified detailPage to add Location & Book Now Widget

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -6,195 +6,301 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: whiteColor,
       body: SafeArea(
-          bottom: false,
-          child: Stack(
-            children: [
-              Image.asset(
-                'assets/thumbnail.png',
-                height: 350,
-                width: MediaQuery.of(context).size.width,
-                fit: BoxFit.cover,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: edge,
-                  vertical: 30,
+        bottom: false,
+        child: Stack(
+          children: [
+            Image.asset(
+              'assets/thumbnail.png',
+              height: 350,
+              width: MediaQuery.of(context).size.width,
+              fit: BoxFit.cover,
+            ),
+            ListView(
+              children: [
+                SizedBox(
+                  height: 328,
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: Image.asset(
-                        'assets/btn_back.png',
-                        width: 40,
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(20),
+                    ),
+                    color: whiteColor,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 30,
+                      ),
+                      //NOTE: TITLE
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: edge,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Kuretakeso Hott',
+                                  style: blackTextStyle.copyWith(
+                                    fontSize: 22,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 2,
+                                ),
+                                Text.rich(TextSpan(
+                                    text: '\$52',
+                                    style: purpleTextStyle.copyWith(
+                                      fontSize: 16,
+                                    ),
+                                    children: [
+                                      TextSpan(
+                                        text: ' / month',
+                                        style: greyTextStyle.copyWith(
+                                          fontSize: 16,
+                                        ),
+                                      )
+                                    ]))
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'assets/Icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/Icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/Icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/Icon_star.png',
+                                  width: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Image.asset(
+                                  'assets/Icon_star.png',
+                                  width: 20,
+                                  color: greyColor,
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      //NOTE: FACILITIES
+                      Padding(
+                        padding: EdgeInsets.only(left: edge),
+                        child: Text(
+                          'Main Facilities',
+                          style: blackTextStyle.copyWith(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: edge),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            FacilityItem(
+                                name: ' kitchen',
+                                imageUrl: 'assets/icon_kitchen.png',
+                                total: 2),
+                            FacilityItem(
+                                name: ' bedroom',
+                                imageUrl: 'assets/icon_badroom.png',
+                                total: 3),
+                            FacilityItem(
+                                name: ' Big Lemari',
+                                imageUrl: 'assets/icon_cupboard.png',
+                                total: 3),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      //NOTE: PHOTO
+                      Padding(
+                        padding: EdgeInsets.only(left: edge),
+                        child: Text(
+                          'Photos',
+                          style: blackTextStyle.copyWith(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Container(
+                        height: 88,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            SizedBox(
+                              width: edge,
+                            ),
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(15),
+                              child: Image.asset(
+                                'assets/photo1.png',
+                                width: 110,
+                                height: 88,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 18,
+                            ),
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(15),
+                              child: Image.asset(
+                                'assets/photo2.png',
+                                width: 110,
+                                height: 88,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 18,
+                            ),
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(15),
+                              child: Image.asset(
+                                'assets/photo3.png',
+                                width: 110,
+                                height: 88,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      //NOTE: LOCATION
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: edge),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Location',
+                              style: blackTextStyle.copyWith(
+                                fontSize: 16,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 6,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Jln. Kappan Sukses No. 20\nPalembang',
+                                  style: greyTextStyle.copyWith(
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                Image.asset(
+                                  'assets/btn_map.png',
+                                  width: 40,
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
                         height: 40,
                       ),
-                    ),
-                    Image.asset(
-                      'assets/btn_wishlist.png',
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: edge),
+                        height: 50,
+                        width: MediaQuery.of(context).size.width - (2 * edge),
+                        child: RaisedButton(
+                          onPressed: () {},
+                          color: purpleColor,
+                          child: Text(
+                            'Book Now',
+                            style: whiteTextStyle.copyWith(
+                              fontSize: 18,
+                            ),
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(17),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: edge,
+                vertical: 30,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Image.asset(
+                      'assets/btn_back.png',
                       width: 40,
                       height: 40,
                     ),
-                  ],
-                ),
-              ),
-              ListView(
-                children: [
-                  SizedBox(
-                    height: 328,
                   ),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.vertical(
-                        top: Radius.circular(20),
-                      ),
-                      color: whiteColor,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        SizedBox(
-                          height: 30,
-                        ),
-                        //NOTE: TITLE
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                            horizontal: edge,
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    'Kuretakeso Hott',
-                                    style: blackTextStyle.copyWith(
-                                      fontSize: 22,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 2,
-                                  ),
-                                  Text.rich(TextSpan(
-                                      text: '\$52',
-                                      style: purpleTextStyle.copyWith(
-                                        fontSize: 16,
-                                      ),
-                                      children: [
-                                        TextSpan(
-                                          text: ' / month',
-                                          style: greyTextStyle.copyWith(
-                                            fontSize: 16,
-                                          ),
-                                        )
-                                      ]))
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  Image.asset(
-                                    'assets/Icon_star.png',
-                                    width: 20,
-                                  ),
-                                  SizedBox(
-                                    width: 2,
-                                  ),
-                                  Image.asset(
-                                    'assets/Icon_star.png',
-                                    width: 20,
-                                  ),
-                                  SizedBox(
-                                    width: 2,
-                                  ),
-                                  Image.asset(
-                                    'assets/Icon_star.png',
-                                    width: 20,
-                                  ),
-                                  SizedBox(
-                                    width: 2,
-                                  ),
-                                  Image.asset(
-                                    'assets/Icon_star.png',
-                                    width: 20,
-                                  ),
-                                  SizedBox(
-                                    width: 2,
-                                  ),
-                                  Image.asset(
-                                    'assets/Icon_star.png',
-                                    width: 20,
-                                    color: greyColor,
-                                  ),
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        //NOTE: FACILITIES
-                        Padding(
-                          padding: EdgeInsets.only(left: edge),
-                          child: Text(
-                            'Main Facilities',
-                            style: blackTextStyle.copyWith(
-                              fontSize: 16,
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 12,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: edge),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              FacilityItem(
-                                  name: ' kitchen',
-                                  imageUrl: 'assets/icon_kitchen.png',
-                                  total: 2),
-                              FacilityItem(
-                                  name: ' bedroom',
-                                  imageUrl: 'assets/icon_badroom.png',
-                                  total: 3),
-                              FacilityItem(
-                                  name: ' Big Lemari',
-                                  imageUrl: 'assets/icon_cupboard.png',
-                                  total: 3),
-                            ],
-                          ),
-                        ),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        //NOTE: PHOTO
-                        Padding(
-                          padding: EdgeInsets.only(left: edge),
-                          child: Text(
-                            'Photos',
-                            style: blackTextStyle.copyWith(
-                              fontSize: 16,
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 12,
-                        ),
-                      ],
-                    ),
-                  )
+                  Image.asset(
+                    'assets/btn_wishlist.png',
+                    width: 40,
+                    height: 40,
+                  ),
                 ],
-              )
-            ],
-          )),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
